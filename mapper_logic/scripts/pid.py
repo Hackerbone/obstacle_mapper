3dabc087e497b6d2b01a399edf76723a69255486
Sitaraman <sitaraman.raghav.india@gmail.com>
2021-07-02 14:13:10 +0530

    Working tracer and PID

--- mapper_logic/scripts/pid.py
+++ mapper_logic/scripts/pid.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 import rospy
-# import sys
-# from std_msgs.msg import String
-# from sensor_msgs.msg import LaserScan
 from sensor_msgs.msg import Range
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
@@ -19,15 +16,25 @@ print(arena_map)
 class mapperClass:
 
     def __init__(self):
+
         self.front = 0
         self.left = 0
         self.right = 0
-        self.direction = 'north'
-        self.prevState = 0
-        self.state = 0
         self.linear = 1
         self.angular = 0
+
+        self.kpAngular = 0.8  # Proportional gain
+        self.kiAngular = 0  # Integral gain
+        self.kdAngular = 8  # Derivative gain
+        self.dt = 0.16689300537109375
+        self.errors = 0         # Integral value
+        self.prevErr = 0        # Derivative value
+        self.imax = 1           # Max integral value
+        self.imin = -1          # Min integral value
         self.vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+
+        # Setpoint distance from wall
+        self.setpoint = 1
 
         self.poseX = 0
         self.poseY = 0
@@ -38,16 +45,17 @@ class mapperClass:
         rospy.Subscriber("/sensor/ir_right", Range, self.subscriber)
         rospy.Subscriber("/sensor/ir_front", Range, self.subscriber)
 
-        # rospy.Subscriber('/odom', Odometry, self.setOdom)
+        rospy.Subscriber('/odom', Odometry, self.setOdom)
 
-    # def setOdom(self, msg):
-    #     self.poseX = msg.pose.pose.position.x
+    def setOdom(self, msg):
+        self.poseX = msg.pose.pose.position.x
 
-    #     self.poseY = msg.pose.pose.position.y
+        self.poseY = msg.pose.pose.position.y
 
-    #     print("POSE X : ", self.poseX)
-    #     print("POSE Y : ", self.poseY)
-    #     arena_map[Math.floor(self.poseX + 10)][Math.floor(self.poseY + 10)] = 1
+        print("POSE X : ", self.poseX)
+        print("POSE Y : ", self.poseY)
+        arena_map[Math.floor(self.poseX + 10)][Math.floor(self.poseY + 10)] = 1
+        print(arena_map)
 
     #     if(self.left == 2):
     #         if(self.poseY + 10 + self.left >= 20):
@@ -69,8 +77,6 @@ class mapperClass:
     #     print(
     #         f'Right Calc : {Math.floor(self.poseY + 10 + self.right)} | {Math.floor(self.poseY + 10 - self.right)} ')
 
-    #     print(arena_map)
-
     def subscriber(self, msg):
         print("\n\n")
         print(f'Front: {self.front} Left: {self.left} Right: {self.right}')
@@ -85,78 +91,63 @@ class mapperClass:
         elif(msg.header.frame_id == "base_ir_right"):
             self.left = msg.range
 
+        self.calculateAngularVel()
+        self.calculateLinearVel()
+
         self.controller()
 
+    def calculateLinearVel(self):
+        # Only proportional control
+        # Based on left distance
+        if(self.error < 1 or self.error > -1):
+            self.linear = 1 - (0.6) * abs(self.error)
+
+        # Based on front distance
+        if(self.front < 3.3):
+            self.linear = 1 - (0.33) * abs(2-self.front)
+
+    def calculateAngularVel(self):
+        # Error term for angular PID
+        self.error = (self.setpoint - self.left)
+
+        # Proportional term
+        self.pAngular = self.kpAngular * self.error
+
+        # Integral term
+        self.errors += self.error
+        if(self.errors > self.imax):
+            self.errors = self.imax
+        elif(self.errors < self.imin):
+            self.errors = self.imin
+        self.iAngular = self.kiAngular * self.errors
+
+        # Derivative Term
+        self.dAngular = self.kdAngular * (self.error - self.prevErr) / self.dt
+        self.prevErr = self.error
+
+        # Total response
+        self.angular = self.pAngular + self.iAngular + self.dAngular
+
+        # Saturating the angular responses
+        if(self.angular > pi/2):
+            self.angular = pi/2
+        elif(self.angular < -1 * pi/2):
+            self.angular = -1*pi/2
+
     def controller(self):
-        # if(self.direction == 'north'):
 
-        if(self.front < 2):
-            self.linear = 0
-            self.angular = 1.57
-            print("FIRST")
-            # rospy.sleep(1.5)
-            # self.direction = 'south'
+        if self.left > 4 and self.front > 4:
+            self.angularResponse = -1 * pi/8
+            self.linear = 0.5
+        # # Front obstacle avoidance
+        if self.front < 3.3:
+            self.angular = 1.8
 
-        elif(self.front == 2):
-
-            if(self.left > 0.5 and self.left < 1.5):
-                self.angular = 0
-
-            elif(self.left <= 0.5):
-                self.angular = 0.5
-
-            elif(self.left >= 1.5):
-                self.angular = -0.5
-
-        if(self.front >= 2 and self.left >= 2):
-            self.angular = -1.57
+        # Linear velocity saturation
+        if self.linear > 1:
+            self.linear = 1
+        if self.linear < 0.1:
             self.linear = 0.1
-
-        # elif(self.direction == 'south'):
-
-        #     if(self.front < 2):
-        #         self.linear = 0
-        #         self.angular = 1.57
-        #         self.state = 3
-        #         # self.direction = 'north'
-        #         print("FIRST")
-
-        #     elif(self.front == 2):
-        #         if(self.state == 3):
-        #             self.state = 0
-        #             self.angular = pi
-        #             self.direction = 'north'
-
-        #         if(self.right > 0.5 and self.right < 1.5):
-        #             self.angular = 0
-
-        #         elif(self.right <= 0.5):
-        #             self.angular = 0.5
-
-        #         elif(self.right >= 1.5):
-        #             self.angular = -0.5
-
-        #         elif(self.left < 2):
-        #             self.angular = 1.57
-
-        #     else:
-        #         self.state = 1
-        #         self.angular = 0
-        #         print("Else")
-
-        # Saturation Cases
-
-        if(self.linear <= 0.2):
-            self.linear = 0.2
-
-        if(self.linear >= 1):
-            self.linear = 1
-
-        if(self.angular >= pi):
-            self.angular = pi
-
-        if(self.front == 2):
-            self.linear = 1
 
         self.vel = Twist()
         self.vel.linear.x = -1*self.linear
